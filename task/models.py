babd1e720468f18023a6b8d022f200a0069b5d19
lassisterv16 <lassisterv16@gmail.com>
2026-06-12 21:28:33 +0200

    added models to commit, changed single quotes with double in apps.py

--- task/models.py
+++ task/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+        null=True,
+        blank=True,
+    )
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "Urgent"
+        HIGH = "High"
+        MEDIUM = "Medium"
+        LOW = "Low"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority,
+        default=Priority.LOW
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
