8a35af2c6efc756a3ff449da6f19b736441b3eb9
lassisterv16 <lassisterv16@gmail.com>
2026-06-18 19:57:27 +0200

    add worker and task forms

---
+++ task/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from task.models import Task
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields + (
+            "position",
+            "first_name",
+            "last_name",
+        )
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        # widgets = {
+        #     "deadline": forms.DateInput(attrs={"type": "date"}),
+        # }
