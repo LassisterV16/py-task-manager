123f7fcb171e3f4b5d0b9a92d0940ca1675f3199
lassisterv16 <lassisterv16@gmail.com>
2026-06-18 20:32:29 +0200

    create index view to main page

--- task/views.py
+++ task/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
+from django.utils import timezone
 from django.shortcuts import render
 
-# Create your views here.
+from task.models import Task
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    today = timezone.localdate()
+
+    num_tasks = Task.objects.count()
+    completed_tasks = Task.objects.filter(is_completed=True).count()
+    tasks_in_progress = Task.objects.filter(
+        is_completed=False, deadline__gte=today
+    ).count()
+    failed_deadlines = Task.objects.filter(
+        is_completed=False, deadline__lt=today
+    ).count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "completed_tasks": completed_tasks,
+        "tasks_in_progress": tasks_in_progress,
+        "failed_deadlines": failed_deadlines,
+    }
+
+    return render(request, "task/index.html", context=context)
