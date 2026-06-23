f299566d354342ff018c9de47f1e122147fcb009
lassisterv16 <lassisterv16@gmail.com>
2026-06-23 21:21:22 +0200

    add task list, task create, update, delete features. add main page

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from task.views import index
+from task.views import index, TaskList, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("task-list/", TaskList.as_view(), name="task-list"),
+    path("task-list/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task-list/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task-list/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
 from django.utils import timezone
 from django.shortcuts import render
+from django.views import generic
 
 from task.models import Task
 
@@ -27,3 +30,32 @@ def index(request: HttpRequest) -> HttpResponse:
     }
 
     return render(request, "task/index.html", context=context)
+
+
+class TaskList(LoginRequiredMixin, generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "task/task_list.html"
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(is_completed=False)
+
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = ("name", "description", "deadline", "priority", "task_type", "assignees",)
+    success_url = reverse_lazy("task:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task:task-list")
