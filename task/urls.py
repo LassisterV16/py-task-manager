4b4e6f937bde0a3604cdd4afa403fc19ad57e561
lassisterv16 <lassisterv16@gmail.com>
2026-06-25 19:26:25 +0200

    add all urls to git

--- task/urls.py
+++ task/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 
-from task.views import index, TaskList, TaskCreateView, TaskUpdateView, TaskDeleteView
+from task.views import (
+    index,
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TaskDetailView,
+    mark_task_completed,
+)
 
 urlpatterns = [
     path("", index, name="index"),
-    path("task-list/", TaskList.as_view(), name="task-list"),
-    path("task-list/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task-list/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("task-list/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("task/<int:pk>/mark-task-completed/", mark_task_completed, name="mark-task-completed"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "task"
