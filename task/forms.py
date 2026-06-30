80e1e7331147246b55a5f4f72f2083c43d3a3763
lassisterv16 <lassisterv16@gmail.com>
2026-06-30 17:51:57 +0200

    deleted label for task search form, added placeholder

--- task/forms.py
+++ task/forms.py
@@ -36,4 +36,13 @@ class TaskUpdateForm(TaskForm):
 
 
 class TaskNameSearchForm(forms.Form):
-    name = forms.CharField(max_length=255, required=False)
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by task name"
+            }
+        )
+    )
